c5b8d4fbb83b4a7e9f7c464af1647b6f37c06632
Sérgio Torres <sergio.ctorres@outlook.com>
2018-04-25 20:51:50 -0300

    Desafio 007 - Staircase

---
+++ 007 - Staircase/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+
+    /*
+     * Complete the staircase function below.
+     */
+    static void staircase(int n)
+    {
+        string spaces = string.Empty;
+        string hashtag = string.Empty;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                spaces += " ";
+            }
+
+            for (int k = 1; k <= i; k++)
+            {
+                hashtag += "#";
+            }
+
+            Console.WriteLine(spaces + hashtag);
+
+            spaces = string.Empty;
+            hashtag = string.Empty;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        staircase(n);
+    }
+}
