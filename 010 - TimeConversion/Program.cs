cc8272afb533cac4309ca47e691ec1c2f393547a
Sérgio Torres <sergio.ctorres@outlook.com>
2018-05-02 21:41:23 -0300

    Desafio 010 - TimeConversion

---
+++ 010 - TimeConversion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    /* Challenge URL: https://www.hackerrank.com/challenges/time-conversion/problem */
+
+    static string timeConversion(string s)
+    {
+        try
+        {
+            DateTime hour = DateTime.Parse(s);
+
+            return hour.ToString("HH:mm:ss");
+        }
+        catch (Exception ex)
+        {
+            return ex.Message;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter tw = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = timeConversion(s);
+
+        tw.WriteLine(result);
+
+        tw.Flush();
+        tw.Close();
+    }
+}
